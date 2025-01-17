70388a6a1562ff0a855c53417b2e14d6ef9c959e
KyawZayHein1 <kyawzayhein528@gmail.com>
2025-01-17 16:54:33 +0630

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+{
+    "name": "ww2-website",
+    "version": "1.0.0",
+    "description": "A brief overview of World War II",
+    "main": "index.html",
+    "scripts": {
+      "start": "live-server"
+    },
+    "author": "Your Name",
+    "license": "ISC",
+    "devDependencies": {
+      "live-server": "^1.2.1"
+    }
+  }
